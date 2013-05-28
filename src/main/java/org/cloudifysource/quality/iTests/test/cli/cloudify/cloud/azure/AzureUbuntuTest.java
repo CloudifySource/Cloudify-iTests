a05e7cb6970876cfba632b4d908f5549ff85d106
inbarc <inbarc@gigaspaces.com>
2013-05-28 15:44:40 +0300

    Add test to bootstrap Ubuntu on Azure (CLOUDIFY-1503)

---
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/azure/AzureUbuntuTest.java
@@ -0,0 +1,35 @@
+package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.azure;
+
+import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.NewAbstractCloudTest;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: inbarc
+ * Date: 5/28/13
+ * Time: 1:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AzureUbuntuTest extends NewAbstractCloudTest {
+
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT , enabled = true)
+    protected void bootstrap() throws Exception {
+        super.bootstrap();
+    }
+    @Override
+    protected void customizeCloud() throws Exception {
+        super.customizeCloud();    //To change body of overridden methods use File | Settings | File Templates.
+        getService().getAdditionalPropsToReplace().put("managementMachineTemplate \"SMALL_LINUX\"", "managementMachineTemplate \"UBUNTU\"")  ;
+    }
+
+
+    @Override
+    protected String getCloudName() {
+        return "azure";
+    }
+
+    @Override
+    protected boolean isReusableCloud() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
